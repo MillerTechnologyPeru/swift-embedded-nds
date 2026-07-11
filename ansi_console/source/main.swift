20bdc066d0d4ee970f93a717e35085fa9aff83d2
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-11 00:41:07 -0400

    Port ansi_console to the NDS overlay

--- ansi_console/source/main.swift
+++ ansi_console/source/main.swift
@@ -4,30 +4,30 @@
 //
 //---------------------------------------------------------------------------------
 
-import CNDS
+import NDS
 
-consoleDemoInit()
+Console.demoInit()
 
 // ansi escape sequence to clear screen and home cursor:  \x1b[2J
-nds_puts("\u{1b}[2J")
+Console.print("\u{1b}[2J")
 
 // ansi escape sequence to set print co-ordinates:        \x1b[line;columnH
-nds_puts("\u{1b}[10;10HHello World!")
+Console.print("\u{1b}[10;10HHello World!")
 
 // ansi escape sequence to move cursor up:                \x1b[linesA
-nds_puts("\u{1b}[10ALine 0")
+Console.print("\u{1b}[10ALine 0")
 
 // ansi escape sequence to move cursor left:              \x1b[columnsD
-nds_puts("\u{1b}[28DColumn 0")
+Console.print("\u{1b}[28DColumn 0")
 
 // ansi escape sequence to move cursor down:              \x1b[linesB
-nds_puts("\u{1b}[19BLine 19")
+Console.print("\u{1b}[19BLine 19")
 
 // ansi escape sequence to move cursor right:             \x1b[columnsC
-nds_puts("\u{1b}[5CColumn 20")
+Console.print("\u{1b}[5CColumn 20")
 
-while pmMainLoop() {
-	threadWaitForVBlank()
-	scanKeys()
-	if keysDown() & KEY_START != 0 { break }
+while System.mainLoop {
+	System.waitForVBlank()
+	Keys.scan()
+	if Keys.down.contains(.start) { break }
 }
