8dd1e96edc56e0b5d423d74cff8562ca53b00c98
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-11 00:41:08 -0400

    Port nehe4 to the NDS overlay

--- nehe4/source/main.swift
+++ nehe4/source/main.swift
@@ -4,59 +4,59 @@
 //
 //---------------------------------------------------------------------------------
 
-import CNDS
+import NDS
 
 var rtri: Float = 0     // angle for the triangle
 var rquad: Float = 0    // angle for the quad
 
 func drawGLScene() {
-	glLoadIdentity()
-	glTranslatef(-1.5, 0.0, -6.0)
-	glRotatef(rtri, 0.0, 1.0, 0.0)        // spin the triangle about Y
-	glColor3f(1, 1, 1)
-	glBegin(GL_TRIANGLES)
-		glColor3f(1.0, 0.0, 0.0)
-		glVertex3f(0.0, 1.0, 0.0)
-		glColor3f(0.0, 1.0, 0.0)
-		glVertex3f(-1.0, -1.0, 0.0)
-		glColor3f(0.0, 0.0, 1.0)
-		glVertex3f(1.0, -1.0, 0.0)
-	glEnd()
-	glLoadIdentity()
-	glTranslatef(1.5, 0.0, -6.0)
-	glRotatef(rquad, 1.0, 0.0, 0.0)       // spin the quad about X
-	glColor3f(0.5, 0.5, 1.0)
-	glBegin(GL_QUADS)
-		glVertex3f(-1.0, 1.0, 0.0)
-		glVertex3f(1.0, 1.0, 0.0)
-		glVertex3f(1.0, -1.0, 0.0)
-		glVertex3f(-1.0, -1.0, 0.0)
-	glEnd()
+	GL.loadIdentity()
+	GL.translate(-1.5, 0.0, -6.0)
+	GL.rotate(rtri, 0.0, 1.0, 0.0)        // spin the triangle about Y
+	GL.color(1, 1, 1)
+	GL.begin(.triangles)
+		GL.color(1.0, 0.0, 0.0)
+		GL.vertex(0.0, 1.0, 0.0)
+		GL.color(0.0, 1.0, 0.0)
+		GL.vertex(-1.0, -1.0, 0.0)
+		GL.color(0.0, 0.0, 1.0)
+		GL.vertex(1.0, -1.0, 0.0)
+	GL.end()
+	GL.loadIdentity()
+	GL.translate(1.5, 0.0, -6.0)
+	GL.rotate(rquad, 1.0, 0.0, 0.0)       // spin the quad about X
+	GL.color(0.5, 0.5, 1.0)
+	GL.begin(.quads)
+		GL.vertex(-1.0, 1.0, 0.0)
+		GL.vertex(1.0, 1.0, 0.0)
+		GL.vertex(1.0, -1.0, 0.0)
+		GL.vertex(-1.0, -1.0, 0.0)
+	GL.end()
 	rtri += 0.9
 	rquad -= 0.75
 }
 
-videoSetMode(MODE_0_3D.rawValue)
-glInit()
-glEnable(Int32(GL_ANTIALIAS.rawValue))
+Video.setMode(.mode0_3D)
+GL.initialize()
+GL.enable(Int32(GL_ANTIALIAS.rawValue))
 
-glClearColor(0, 0, 0, 31)
-glClearPolyID(63)
-glClearDepth(0x7FFF)
+GL.clearColor(r: 0, g: 0, b: 0, a: 31)
+GL.clearPolyID(63)
+GL.clearDepth(0x7FFF)
 
-glViewport(0, 0, 255, 191)
+GL.viewport(0, 0, 255, 191)
 
-glMatrixMode(GL_PROJECTION)
-glLoadIdentity()
-gluPerspective(70, 256.0 / 192.0, 0.1, 100)
+GL.matrixMode(.projection)
+GL.loadIdentity()
+GL.perspective(fovy: 70, aspect: 256.0 / 192.0, near: 0.1, far: 100)
 
-glPolyFmt(POLY_ALPHA(31) | UInt32(POLY_CULL_NONE.rawValue))
-glMatrixMode(GL_MODELVIEW)
+GL.polyFmt(POLY_ALPHA(31) | UInt32(POLY_CULL_NONE.rawValue))
+GL.matrixMode(.modelview)
 
-while pmMainLoop() {
+while System.mainLoop {
 	drawGLScene()
-	glFlush(0)
-	threadWaitForVBlank()
-	scanKeys()
-	if keysDown() & KEY_START != 0 { break }
+	GL.flush()
+	System.waitForVBlank()
+	Keys.scan()
+	if Keys.down.contains(.start) { break }
 }
