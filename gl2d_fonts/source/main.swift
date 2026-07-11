740dc4fa4aa9d07fd8c65951d3d5de97eceaf4a8
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-11 00:41:08 -0400

    Port gl2d_fonts to the NDS overlay

--- gl2d_fonts/source/main.swift
+++ gl2d_fonts/source/main.swift
@@ -8,10 +8,10 @@
 //
 //---------------------------------------------------------------------------------
 
-import CNDS
+import NDS
 
-@inline(__always) func rgb15(_ r: Int32, _ g: Int32, _ b: Int32) -> UInt16 {
-	UInt16(truncatingIfNeeded: r | (g << 5) | (b << 10))
+@inline(__always) func rgb15(_ r: Int32, _ g: Int32, _ b: Int32) -> Color {
+	Color(rawValue: UInt16(truncatingIfNeeded: r | (g << 5) | (b << 10)))
 }
 
 // A bitmap font: one glImage frame per glyph (ascii - 32).
@@ -24,8 +24,9 @@ struct GLFont {
 	                   type: GL_TEXTURE_TYPE_ENUM, sizeX: Int32, sizeY: Int32, param: Int32,
 	                   paletteWidth: Int32, palette: UnsafePointer<UInt16>, texture: UnsafePointer<UInt8>) {
 		images.withUnsafeMutableBufferPointer { buf in
-			_ = glLoadSpriteSet(buf.baseAddress, numframes, texcoords, type,
-			                    sizeX, sizeY, param, paletteWidth, palette, texture)
+			GL2D.loadSpriteSet(buf.baseAddress!, frames: numframes, texcoords: texcoords, type: type,
+			                   sizeX: sizeX, sizeY: sizeY, param: param, paletteWidth: paletteWidth,
+			                   palette: palette, texture: texture)
 		}
 	}
 
@@ -34,7 +35,7 @@ struct GLFont {
 		images.withUnsafeBufferPointer { buf in
 			for byte in text.utf8 {
 				let fc = Int(byte) - 32
-				glSprite(x, y, Int32(GL_FLIP_NONE.rawValue), buf.baseAddress! + fc)
+				GL2D.sprite(x: x, y: y, flip: Int32(GL_FLIP_NONE.rawValue), buf.baseAddress! + fc)
 				x += buf[fc].width
 			}
 		}
@@ -50,12 +51,12 @@ struct GLFont {
 let texParam = Int32(GL_TEXTURE_WRAP_S.rawValue | GL_TEXTURE_WRAP_T.rawValue
                      | TEXGEN_OFF.rawValue | GL_TEXTURE_COLOR0_TRANSPARENT.rawValue)
 
-videoSetMode(MODE_5_3D.rawValue)
-consoleDemoInit()
-glScreen2D()
+Video.setMode(.mode5_3D)
+Console.demoInit()
+GL2D.screen2D()
 
-vramSetBankA(VRAM_A_TEXTURE)
-vramSetBankE(VRAM_E_TEX_PALETTE)
+Video.setBankA(VRAM_A_TEXTURE)
+Video.setBankE(VRAM_E_TEX_PALETTE)
 
 let pal = nds_asset_font_siPal()!.assumingMemoryBound(to: UInt16.self)
 
@@ -75,50 +76,50 @@ fontBig.load(numframes: UInt32(FONT_16X16_NUM_IMAGES),
              param: texParam, paletteWidth: 256, palette: pal,
              texture: nds_asset_font_16x16Bitmap()!.assumingMemoryBound(to: UInt8.self))
 
-nds_puts("\u{1b}[1;1HEasy GL2D Font Example")
-nds_puts("\u{1b}[3;1HFonts by Adigun A. Polack")
-nds_puts("\u{1b}[6;1HRelminator")
-nds_puts("\u{1b}[7;1HHttp://Rel.Phatcode.Net")
-nds_printf_1i("\u{1b}[10;1HTotal Texture size= %i kb",
-              Int32(font_siBitmapLen + font_16x16BitmapLen) / 1024)
+Console.print("\u{1b}[1;1HEasy GL2D Font Example")
+Console.print("\u{1b}[3;1HFonts by Adigun A. Polack")
+Console.print("\u{1b}[6;1HRelminator")
+Console.print("\u{1b}[7;1HHttp://Rel.Phatcode.Net")
+Console.printf("\u{1b}[10;1HTotal Texture size= %i kb",
+               Int32(font_siBitmapLen + font_16x16BitmapLen) / 1024)
 
 var frame: Int32 = 0
 
-while pmMainLoop() {
+while System.mainLoop {
 	frame += 1
 
-	glBegin2D()
+	GL2D.begin2D()
 
-	glBoxFilledGradient(0, 0, 255, 191,
-	                    Int32(rgb15(31, 0, 0)), Int32(rgb15(0, 31, 0)),
-	                    Int32(rgb15(31, 0, 31)), Int32(rgb15(0, 31, 31)))
+	GL2D.boxFilledGradient(x1: 0, y1: 0, x2: 255, y2: 191,
+	                       color1: Int32(rgb15(31, 0, 0).rawValue), color2: Int32(rgb15(0, 31, 0).rawValue),
+	                       color3: Int32(rgb15(31, 0, 31).rawValue), color4: Int32(rgb15(0, 31, 31).rawValue))
 
-	glColor(rgb15(0, 0, 0))
+	GL.color(rgb15(0, 0, 0))
 	fontBig.printCentered(0, "EASY GL2D")
-	glColor(rgb15((frame * 6) & 31, (-frame * 4) & 31, (frame * 2) & 31))
+	GL.color(rgb15((frame * 6) & 31, (-frame * 4) & 31, (frame * 2) & 31))
 	fontBig.printCentered(20, "FONT EXAMPLE")
 
-	let x = (Int32(sinLerp(Int16(truncatingIfNeeded: frame &* 400))) * 30) >> 12
-	glColor(rgb15(31, 0, 0))
+	let x = (Int32(Math.sin(Int16(truncatingIfNeeded: frame &* 400))) * 30) >> 12
+	GL.color(rgb15(31, 0, 0))
 	fontBig.print(25 + x, 50, "hfDEVKITPROfh")
-	glColor(rgb15(x, 31 - x, x * 2))
+	GL.color(rgb15(x, 31 - x, x * 2))
 	fontBig.print(50 - x, 70, "dcLIBNDScd")
 
-	glColor(rgb15(0, 31, 31))
+	GL.color(rgb15(0, 31, 31))
 	font.printCentered(100, "FONTS BY ADIGUN A. POLACK")
 	font.printCentered(120, "CODE BY RELMINATOR")
 
-	glColor(rgb15(31, 31, 31))
-	let opacity = abs(Int32(sinLerp(Int16(truncatingIfNeeded: frame &* 245))) * 30) >> 12
-	glPolyFmt(POLY_ALPHA(UInt32(1 + opacity)) | UInt32(POLY_CULL_NONE.rawValue) | POLY_ID(1))
+	GL.color(rgb15(31, 31, 31))
+	let opacity = abs(Int32(Math.sin(Int16(truncatingIfNeeded: frame &* 245))) * 30) >> 12
+	GL.polyFmt(POLY_ALPHA(UInt32(1 + opacity)) | UInt32(POLY_CULL_NONE.rawValue) | POLY_ID(1))
 	fontBig.print(35 + x, 140, "ANYA THERESE")
 
-	glPolyFmt(POLY_ALPHA(31) | UInt32(POLY_CULL_NONE.rawValue) | POLY_ID(2))
+	GL.polyFmt(POLY_ALPHA(31) | UInt32(POLY_CULL_NONE.rawValue) | POLY_ID(2))
 	font.print(10, 170, "FRAMES = ")
 
-	glEnd2D()
-	glFlush(0)
-	threadWaitForVBlank()
-	scanKeys()
-	if keysDown() & KEY_START != 0 { break }
+	GL2D.end2D()
+	GL.flush()
+	System.waitForVBlank()
+	Keys.scan()
+	if Keys.down.contains(.start) { break }
 }
