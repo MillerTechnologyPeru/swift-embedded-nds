19bf9be6f5bad563fb5aebf1bce9d69beba75e3c
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-11 00:41:09 -0400

    Port nehe8 to the NDS overlay

--- nehe8/source/main.swift
+++ nehe8/source/main.swift
@@ -7,14 +7,7 @@
 //
 //---------------------------------------------------------------------------------
 
-import CNDS
-
-@inline(__always) func rgb15(_ r: UInt16, _ g: UInt16, _ b: UInt16) -> UInt16 {
-	r | (g << 5) | (b << 10)
-}
-@inline(__always) func floattov10(_ n: Float) -> Int16 {
-	n > 0.998 ? 0x1FF : Int16(n * Float(1 << 9))
-}
+import NDS
 
 var xrot: Float = 0, yrot: Float = 0
 var xspeed: Float = 0, yspeed: Float = 0
@@ -25,99 +18,99 @@ func loadGLTextures() {
 	var pcx = sImage()
 	loadPCX(nds_asset_drunkenlogo_pcx()!.assumingMemoryBound(to: UInt8.self), &pcx)
 	image8to16(&pcx)
-	glGenTextures(1, &texture0)
-	glBindTexture(0, texture0)
-	glTexImage2D(0, 0, GL_RGB, Int32(TEXTURE_SIZE_128.rawValue), Int32(TEXTURE_SIZE_128.rawValue),
-	             0, Int32(TEXGEN_TEXCOORD.rawValue), pcx.image.data8)
+	_ = GL.genTextures(1, &texture0)
+	GL.bindTexture(0, texture0)
+	_ = GL.texImage2D(target: 0, type: GL_RGB, sizeX: Int32(TEXTURE_SIZE_128.rawValue), sizeY: Int32(TEXTURE_SIZE_128.rawValue),
+	                  param: Int32(TEXGEN_TEXCOORD.rawValue), texture: pcx.image.data8)
 	imageDestroy(&pcx)
 }
 
 func drawGLScene() {
-	glLoadIdentity()
-	glTranslatef(0, 0, z)
-	glRotatef(xrot, 1, 0, 0)
-	glRotatef(yrot, 0, 1, 0)
-	glPolyFmt(POLY_ALPHA(31) | UInt32(POLY_CULL_NONE.rawValue) | UInt32(POLY_FORMAT_LIGHT0.rawValue))
-	glBindTexture(Int32(GL_TEXTURE_2D.rawValue), texture0)
-	glBegin(GL_QUADS)
-		glNormal3f(0, 0, 1)
-		glTexCoord2f(0, 0); glVertex3f(-1, -1, 1)
-		glTexCoord2f(1, 0); glVertex3f(1, -1, 1)
-		glTexCoord2f(1, 1); glVertex3f(1, 1, 1)
-		glTexCoord2f(0, 1); glVertex3f(-1, 1, 1)
-		glNormal3f(0, 0, -1)
-		glTexCoord2f(1, 0); glVertex3f(-1, -1, -1)
-		glTexCoord2f(1, 1); glVertex3f(-1, 1, -1)
-		glTexCoord2f(0, 1); glVertex3f(1, 1, -1)
-		glTexCoord2f(0, 0); glVertex3f(1, -1, -1)
-		glNormal3f(0, 1, 0)
-		glTexCoord2f(0, 1); glVertex3f(-1, 1, -1)
-		glTexCoord2f(0, 0); glVertex3f(-1, 1, 1)
-		glTexCoord2f(1, 0); glVertex3f(1, 1, 1)
-		glTexCoord2f(1, 1); glVertex3f(1, 1, -1)
-		glNormal3f(0, -1, 0)
-		glTexCoord2f(1, 1); glVertex3f(-1, -1, -1)
-		glTexCoord2f(0, 1); glVertex3f(1, -1, -1)
-		glTexCoord2f(0, 0); glVertex3f(1, -1, 1)
-		glTexCoord2f(1, 0); glVertex3f(-1, -1, 1)
-		glNormal3f(1, 0, 0)
-		glTexCoord2f(1, 0); glVertex3f(1, -1, -1)
-		glTexCoord2f(1, 1); glVertex3f(1, 1, -1)
-		glTexCoord2f(0, 1); glVertex3f(1, 1, 1)
-		glTexCoord2f(0, 0); glVertex3f(1, -1, 1)
-	glEnd()
-	glPolyFmt(POLY_ALPHA(15) | UInt32(POLY_CULL_BACK.rawValue) | UInt32(POLY_FORMAT_LIGHT0.rawValue))
-	glBegin(GL_QUADS)
-		glNormal3f(-1, 0, 0)
-		glTexCoord2f(0, 0); glVertex3f(-1, -1, -1)
-		glTexCoord2f(1, 0); glVertex3f(-1, -1, 1)
-		glTexCoord2f(1, 1); glVertex3f(-1, 1, 1)
-		glTexCoord2f(0, 1); glVertex3f(-1, 1, -1)
-	glEnd()
+	GL.loadIdentity()
+	GL.translate(0, 0, z)
+	GL.rotate(xrot, 1, 0, 0)
+	GL.rotate(yrot, 0, 1, 0)
+	GL.polyFmt(POLY_ALPHA(31) | UInt32(POLY_CULL_NONE.rawValue) | UInt32(POLY_FORMAT_LIGHT0.rawValue))
+	GL.bindTexture(Int32(GL_TEXTURE_2D.rawValue), texture0)
+	GL.begin(.quads)
+		GL.normal(0, 0, 1)
+		GL.texCoord(0, 0); GL.vertex(-1, -1, 1)
+		GL.texCoord(1, 0); GL.vertex(1, -1, 1)
+		GL.texCoord(1, 1); GL.vertex(1, 1, 1)
+		GL.texCoord(0, 1); GL.vertex(-1, 1, 1)
+		GL.normal(0, 0, -1)
+		GL.texCoord(1, 0); GL.vertex(-1, -1, -1)
+		GL.texCoord(1, 1); GL.vertex(-1, 1, -1)
+		GL.texCoord(0, 1); GL.vertex(1, 1, -1)
+		GL.texCoord(0, 0); GL.vertex(1, -1, -1)
+		GL.normal(0, 1, 0)
+		GL.texCoord(0, 1); GL.vertex(-1, 1, -1)
+		GL.texCoord(0, 0); GL.vertex(-1, 1, 1)
+		GL.texCoord(1, 0); GL.vertex(1, 1, 1)
+		GL.texCoord(1, 1); GL.vertex(1, 1, -1)
+		GL.normal(0, -1, 0)
+		GL.texCoord(1, 1); GL.vertex(-1, -1, -1)
+		GL.texCoord(0, 1); GL.vertex(1, -1, -1)
+		GL.texCoord(0, 0); GL.vertex(1, -1, 1)
+		GL.texCoord(1, 0); GL.vertex(-1, -1, 1)
+		GL.normal(1, 0, 0)
+		GL.texCoord(1, 0); GL.vertex(1, -1, -1)
+		GL.texCoord(1, 1); GL.vertex(1, 1, -1)
+		GL.texCoord(0, 1); GL.vertex(1, 1, 1)
+		GL.texCoord(0, 0); GL.vertex(1, -1, 1)
+	GL.end()
+	GL.polyFmt(POLY_ALPHA(15) | UInt32(POLY_CULL_BACK.rawValue) | UInt32(POLY_FORMAT_LIGHT0.rawValue))
+	GL.begin(.quads)
+		GL.normal(-1, 0, 0)
+		GL.texCoord(0, 0); GL.vertex(-1, -1, -1)
+		GL.texCoord(1, 0); GL.vertex(-1, -1, 1)
+		GL.texCoord(1, 1); GL.vertex(-1, 1, 1)
+		GL.texCoord(0, 1); GL.vertex(-1, 1, -1)
+	GL.end()
 	xrot += xspeed
 	yrot += yspeed
 }
 
-videoSetMode(MODE_0_3D.rawValue)
-vramSetBankA(VRAM_A_TEXTURE)
-glInit()
-glEnable(Int32(GL_TEXTURE_2D.rawValue))
-glEnable(Int32(GL_BLEND.rawValue))
-glEnable(Int32(GL_ANTIALIAS.rawValue))
+Video.setMode(.mode0_3D)
+Video.setBankA(VRAM_A_TEXTURE)
+GL.initialize()
+GL.enable(Int32(GL_TEXTURE_2D.rawValue))
+GL.enable(Int32(GL_BLEND.rawValue))
+GL.enable(Int32(GL_ANTIALIAS.rawValue))
 
-glClearColor(0, 0, 0, 31)
-glClearPolyID(63)
-glClearDepth(0x7FFF)
-glViewport(0, 0, 255, 191)
+GL.clearColor(r: 0, g: 0, b: 0, a: 31)
+GL.clearPolyID(63)
+GL.clearDepth(0x7FFF)
+GL.viewport(0, 0, 255, 191)
 
 loadGLTextures()
 
-glMatrixMode(GL_PROJECTION)
-glLoadIdentity()
-gluPerspective(70, 256.0 / 192.0, 0.1, 100)
+GL.matrixMode(.projection)
+GL.loadIdentity()
+GL.perspective(fovy: 70, aspect: 256.0 / 192.0, near: 0.1, far: 100)
 
-glColor3f(1, 1, 1)
-glLight(0, rgb15(31, 31, 31), 0, floattov10(-1.0), 0)
-glMaterialf(GL_AMBIENT, rgb15(8, 8, 8))
-glMaterialf(GL_DIFFUSE, rgb15(8, 8, 8))
-glMaterialf(GL_SPECULAR, (UInt16(1) << 15) | rgb15(8, 8, 8))
-glMaterialf(GL_EMISSION, rgb15(16, 16, 16))
-glMaterialShinyness()
-glMatrixMode(GL_MODELVIEW)
+GL.color(1, 1, 1)
+GL.light(0, color: Color(r: 31, g: 31, b: 31), x: 0, y: floattov10(-1.0), z: 0)
+GL.material(GL_AMBIENT, Color(r: 8, g: 8, b: 8))
+GL.material(GL_DIFFUSE, Color(r: 8, g: 8, b: 8))
+GL.material(GL_SPECULAR, Color(rawValue: (UInt16(1) << 15) | Color(r: 8, g: 8, b: 8).rawValue))
+GL.material(GL_EMISSION, Color(r: 16, g: 16, b: 16))
+GL.materialShininess()
+GL.matrixMode(.modelview)
 
-while pmMainLoop() {
-	scanKeys()
-	let held = keysHeld()
-	let pressed = keysDown()
-	if held & KEY_R != 0 { z -= 0.02 }
-	if held & KEY_L != 0 { z += 0.02 }
-	if held & KEY_LEFT != 0 { xspeed -= 0.01 }
-	if held & KEY_RIGHT != 0 { xspeed += 0.01 }
-	if held & KEY_UP != 0 { yspeed += 0.01 }
-	if held & KEY_DOWN != 0 { yspeed -= 0.01 }
+while System.mainLoop {
+	Keys.scan()
+	let held = Keys.held
+	let pressed = Keys.down
+	if held.contains(.r) { z -= 0.02 }
+	if held.contains(.l) { z += 0.02 }
+	if held.contains(.left) { xspeed -= 0.01 }
+	if held.contains(.right) { xspeed += 0.01 }
+	if held.contains(.up) { yspeed += 0.01 }
+	if held.contains(.down) { yspeed -= 0.01 }
 
 	drawGLScene()
-	glFlush(0)
-	threadWaitForVBlank()
-	if pressed & KEY_START != 0 { break }
+	GL.flush()
+	System.waitForVBlank()
+	if pressed.contains(.start) { break }
 }
