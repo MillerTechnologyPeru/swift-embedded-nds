44d38779fc618ad69377e48a24098e5b48422d41
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-11 00:41:07 -0400

    Port gl2d_2dplus3d to the NDS overlay

--- gl2d_2dplus3d/source/main.swift
+++ gl2d_2dplus3d/source/main.swift
@@ -8,20 +8,20 @@
 //
 //---------------------------------------------------------------------------------
 
-import CNDS
+import NDS
 
 let GRID_RINGS = 2, GRID_BANDS = 2
 let GRID_WIDTH: Float = 15, GRID_HEIGHT: Float = 12
 let BRAD_PI: Int32 = 1 << 14
+let FLIP_NONE = Int32(GL_FLIP_NONE.rawValue)
+let FLIP_H = Int32(GL_FLIP_H.rawValue)
+let FLIP_V = Int32(GL_FLIP_V.rawValue)
 
-@inline(__always) func rgb15(_ r: Int32, _ g: Int32, _ b: Int32) -> UInt16 {
-	UInt16(truncatingIfNeeded: r | (g << 5) | (b << 10))
+@inline(__always) func rgb15(_ r: Int32, _ g: Int32, _ b: Int32) -> Color {
+	Color(rawValue: UInt16(truncatingIfNeeded: r | (g << 5) | (b << 10)))
 }
-@inline(__always) func slerp(_ a: Int32) -> Int32 { Int32(sinLerp(Int16(truncatingIfNeeded: a))) }
-@inline(__always) func clerp(_ a: Int32) -> Int32 { Int32(cosLerp(Int16(truncatingIfNeeded: a))) }
-@inline(__always) func floattof32(_ n: Float) -> Int32 { Int32(n * Float(1 << 12)) }
-@inline(__always) func floattov16(_ n: Float) -> Int16 { Int16(truncatingIfNeeded: Int32(n * Float(1 << 12))) }
-@inline(__always) func inttof32(_ n: Int32) -> Int32 { n * (1 << 12) }
+@inline(__always) func slerp(_ a: Int32) -> Int32 { Int32(Math.sin(Int16(truncatingIfNeeded: a))) }
+@inline(__always) func clerp(_ a: Int32) -> Int32 { Int32(Math.cos(Int16(truncatingIfNeeded: a))) }
 
 // Ported from the example's C++ Cvertexbuffer class (the parts 2Dplus3D uses).
 struct Vec3 { var x: Int16 = 0, y: Int16 = 0, z: Int16 = 0 }
@@ -38,28 +38,28 @@ final class VertexBuffer {
 	var poly: [Poly] = []
 
 	func loadTexture(_ gfx: UnsafePointer<UInt8>) {
-		glGenTextures(1, &textureID)
-		glBindTexture(0, textureID)
-		glTexImage2D(0, 0, GL_RGB, Int32(TEXTURE_SIZE_128.rawValue), Int32(TEXTURE_SIZE_128.rawValue),
-		             0, Int32(GL_TEXTURE_WRAP_S.rawValue | GL_TEXTURE_WRAP_T.rawValue | TEXGEN_TEXCOORD.rawValue), gfx)
+		_ = GL.genTextures(1, &textureID)
+		GL.bindTexture(0, textureID)
+		_ = GL.texImage2D(target: 0, type: GL_RGB, sizeX: Int32(TEXTURE_SIZE_128.rawValue), sizeY: Int32(TEXTURE_SIZE_128.rawValue),
+		                  param: Int32(GL_TEXTURE_WRAP_S.rawValue | GL_TEXTURE_WRAP_T.rawValue | TEXGEN_TEXCOORD.rawValue), texture: gfx)
 	}
 
 	func render(_ offU: Int32, _ offV: Int32, _ colorize: Bool) {
-		glEnable(Int32(GL_TEXTURE_2D.rawValue))
-		glBindTexture(0, textureID)
-		glBegin(GL_TRIANGLES)
+		GL.enable(Int32(GL_TEXTURE_2D.rawValue))
+		GL.bindTexture(0, textureID)
+		GL.begin(.triangles)
 		for i in 0 ..< maxPoly {
 			for vi in [poly[i].v1, poly[i].v2, poly[i].v3] {
 				if colorize {
-					glColor3b(UInt8(truncatingIfNeeded: color[vi].r),
-					          UInt8(truncatingIfNeeded: color[vi].g),
-					          UInt8(truncatingIfNeeded: color[vi].b))
+					GL.color(r: UInt8(truncatingIfNeeded: color[vi].r),
+					         g: UInt8(truncatingIfNeeded: color[vi].g),
+					         b: UInt8(truncatingIfNeeded: color[vi].b))
 				}
-				glTexCoord2f32(texcoord[vi].u + offU, texcoord[vi].v + offV)
-				glVertex3v16(vertex[vi].x, vertex[vi].y, vertex[vi].z)
+				GL.texCoordf32(texcoord[vi].u + offU, texcoord[vi].v + offV)
+				GL.vertex16(vertex[vi].x, vertex[vi].y, vertex[vi].z)
 			}
 		}
-		glEnd()
+		GL.end()
 	}
 }
 
@@ -119,38 +119,38 @@ var flyer = [glImage](repeating: glImage(), count: 1)
 
 let vb = initGrid(rings: GRID_RINGS, bands: GRID_BANDS, width: GRID_WIDTH, height: GRID_HEIGHT, uscale: 8, vscale: 8)
 
-videoSetMode(MODE_5_3D.rawValue)
-consoleDemoInit()
-glScreen2D()
-vramSetBankA(VRAM_A_TEXTURE)
-vramSetBankE(VRAM_E_TEX_PALETTE)
+Video.setMode(.mode5_3D)
+Console.demoInit()
+GL2D.screen2D()
+Video.setBankA(VRAM_A_TEXTURE)
+Video.setBankE(VRAM_E_TEX_PALETTE)
 
 vb.loadTexture(nds_asset_organ16Bitmap()!.assumingMemoryBound(to: UInt8.self))
 
 enemies.withUnsafeMutableBufferPointer { buf in
-	_ = glLoadSpriteSet(buf.baseAddress, UInt32(ENEMIES_NUM_IMAGES),
-	                    nds_asset_enemies_texcoords()!.assumingMemoryBound(to: UInt32.self),
-	                    GL_RGB256, Int32(TEXTURE_SIZE_256.rawValue), Int32(TEXTURE_SIZE_256.rawValue),
-	                    texParam, 256, nds_asset_enemiesPal()!.assumingMemoryBound(to: UInt16.self),
-	                    nds_asset_enemiesBitmap()!.assumingMemoryBound(to: UInt8.self))
+	GL2D.loadSpriteSet(buf.baseAddress!, frames: UInt32(ENEMIES_NUM_IMAGES),
+	                   texcoords: nds_asset_enemies_texcoords()!.assumingMemoryBound(to: UInt32.self),
+	                   type: GL_RGB256, sizeX: Int32(TEXTURE_SIZE_256.rawValue), sizeY: Int32(TEXTURE_SIZE_256.rawValue),
+	                   param: texParam, paletteWidth: 256, palette: nds_asset_enemiesPal()!.assumingMemoryBound(to: UInt16.self),
+	                   texture: nds_asset_enemiesBitmap()!.assumingMemoryBound(to: UInt8.self))
 }
 shuttle.withUnsafeMutableBufferPointer { buf in
-	_ = glLoadTileSet(buf.baseAddress, 64, 64, 64, 64, GL_RGB16,
-	                  Int32(TEXTURE_SIZE_64.rawValue), Int32(TEXTURE_SIZE_64.rawValue),
-	                  texParam, 16, nds_asset_shuttlePal()!.assumingMemoryBound(to: UInt16.self),
-	                  nds_asset_shuttleBitmap()!.assumingMemoryBound(to: UInt8.self))
+	GL2D.loadTileSet(buf.baseAddress!, tileWidth: 64, tileHeight: 64, bmpWidth: 64, bmpHeight: 64, type: GL_RGB16,
+	                 sizeX: Int32(TEXTURE_SIZE_64.rawValue), sizeY: Int32(TEXTURE_SIZE_64.rawValue),
+	                 param: texParam, paletteWidth: 16, palette: nds_asset_shuttlePal()!.assumingMemoryBound(to: UInt16.self),
+	                 texture: nds_asset_shuttleBitmap()!.assumingMemoryBound(to: UInt8.self))
 }
 flyer.withUnsafeMutableBufferPointer { buf in
-	_ = glLoadTileSet(buf.baseAddress, 64, 64, 64, 64, GL_RGB16,
-	                  Int32(TEXTURE_SIZE_64.rawValue), Int32(TEXTURE_SIZE_64.rawValue),
-	                  texParam, 16, nds_asset_flyerPal()!.assumingMemoryBound(to: UInt16.self),
-	                  nds_asset_flyerBitmap()!.assumingMemoryBound(to: UInt8.self))
+	GL2D.loadTileSet(buf.baseAddress!, tileWidth: 64, tileHeight: 64, bmpWidth: 64, bmpHeight: 64, type: GL_RGB16,
+	                 sizeX: Int32(TEXTURE_SIZE_64.rawValue), sizeY: Int32(TEXTURE_SIZE_64.rawValue),
+	                 param: texParam, paletteWidth: 16, palette: nds_asset_flyerPal()!.assumingMemoryBound(to: UInt16.self),
+	                 texture: nds_asset_flyerBitmap()!.assumingMemoryBound(to: UInt8.self))
 }
 
-nds_puts("\u{1b}[1;1HEasy GL2D + 3D")
-nds_puts("\u{1b}[2;1HRelminator")
-nds_puts("\u{1b}[4;1HHttp://Rel.Phatcode.Net")
-nds_puts("\u{1b}[6;1HCombining 3D and 2D")
+Console.print("\u{1b}[1;1HEasy GL2D + 3D")
+Console.print("\u{1b}[2;1HRelminator")
+Console.print("\u{1b}[4;1HHttp://Rel.Phatcode.Net")
+Console.print("\u{1b}[6;1HCombining 3D and 2D")
 
 var gridU: Int32 = 0
 var gridV: Int32 = 0
@@ -158,21 +158,21 @@ var gridFrame: Int32 = 0
 
 func drawGrid() {
 	gridFrame += 1
-	glPushMatrix()
-	glLoadIdentity()
-	glTranslatef32(0, 0, floattof32(3.0))
-	glScalef32(floattof32(1), floattof32(1.5), floattof32(1.5))
-	glPushMatrix()
-	glTranslatef32(0, -1 << 12, -2 << 12)
-	glRotateXi(inttof32(244))
+	GL.pushMatrix()
+	GL.loadIdentity()
+	GL.translatef32(0, 0, floattof32(3.0))
+	GL.scalef32(floattof32(1), floattof32(1.5), floattof32(1.5))
+	GL.pushMatrix()
+	GL.translatef32(0, -1 << 12, -2 << 12)
+	GL.rotateXi(inttof32(244))
 	vb.render(gridU, gridV, true)
-	glPopMatrix(1)
-	glPushMatrix()
-	glTranslatef32(0, 1 << 12, -2 << 12)
-	glRotateXi(inttof32(180))
+	GL.popMatrix()
+	GL.pushMatrix()
+	GL.translatef32(0, 1 << 12, -2 << 12)
+	GL.rotateXi(inttof32(180))
 	vb.render(gridU, gridV, true)
-	glPopMatrix(1)
-	glPopMatrix(1)
+	GL.popMatrix()
+	GL.popMatrix()
 	gridU = (slerp(gridFrame &* 30) &* 2) & 4095
 	gridV = (slerp(-gridFrame &* 50) &* 3) & 4095
 }
@@ -182,7 +182,7 @@ var frame: Int32 = 0
 var phoenixFrame = 0
 var beeFrame: Int32 = 0
 
-while pmMainLoop() {
+while System.mainLoop {
 	frame += 1
 	let rotation = frame &* 240
 	if frame & 7 == 0 {
@@ -195,43 +195,43 @@ while pmMainLoop() {
 	let y = 96 + ((clerp(frame) + clerp(-rotation) &* 50) >> 12)
 	var sx = (clerp(frame &* 150) + slerp(frame &* 70)) &* (256 / 4)
 	var sy = (slerp(-(frame &* 80)) + slerp(frame &* 190)) &* (192 / 4)
-	let angle = Int32(bitPattern: nds_atan2_lerp(ox - sx, oy - sy))
+	let angle = Int32(bitPattern: Math.atan2(x: ox - sx, y: oy - sy))
 	ox = sx; oy = sy
 	sx = 128 + (sx >> 12)
 	sy = 96 + (sy >> 12)
 
 	drawGrid()
 
-	glBegin2D()
+	GL2D.begin2D()
 	enemies.withUnsafeBufferPointer { e in
-		glSpriteRotate(x, y, rotation, Int32(GL_FLIP_NONE.rawValue), e.baseAddress! + 30 + Int(beeFrame))
-		glSpriteRotate(255 - x, 191 - y, rotation &* 4, Int32(GL_FLIP_H.rawValue), e.baseAddress! + 84)
-		glSpriteRotate(255 - x, y, -rotation, Int32(GL_FLIP_V.rawValue), e.baseAddress! + 32)
-		glSpriteRotate(x, 191 - y, -rotation &* 3, Int32(GL_FLIP_H.rawValue | GL_FLIP_V.rawValue), e.baseAddress! + 81)
-		glSprite(200, 30, Int32(GL_FLIP_NONE.rawValue), e.baseAddress! + 87 + phoenixFrame)
-		glColor(rgb15(31, 0, 0))
-		glSprite(200, 60, Int32(GL_FLIP_H.rawValue), e.baseAddress! + 87 + phoenixFrame)
-		glPolyFmt(POLY_ALPHA(20) | UInt32(POLY_CULL_NONE.rawValue) | POLY_ID(1))
-		glColor(rgb15(0, 31, 20))
-		glSprite(200, 90, Int32(GL_FLIP_V.rawValue), e.baseAddress! + 87 + phoenixFrame)
-		glColor(rgb15(0, 0, 0))
-		glSprite(200, 130, Int32(GL_FLIP_V.rawValue | GL_FLIP_H.rawValue), e.baseAddress! + 87 + phoenixFrame)
+		GL2D.spriteRotate(x: x, y: y, angle: rotation, flip: FLIP_NONE, e.baseAddress! + 30 + Int(beeFrame))
+		GL2D.spriteRotate(x: 255 - x, y: 191 - y, angle: rotation &* 4, flip: FLIP_H, e.baseAddress! + 84)
+		GL2D.spriteRotate(x: 255 - x, y: y, angle: -rotation, flip: FLIP_V, e.baseAddress! + 32)
+		GL2D.spriteRotate(x: x, y: 191 - y, angle: -rotation &* 3, flip: FLIP_H | FLIP_V, e.baseAddress! + 81)
+		GL2D.sprite(x: 200, y: 30, flip: FLIP_NONE, e.baseAddress! + 87 + phoenixFrame)
+		GL.color(rgb15(31, 0, 0))
+		GL2D.sprite(x: 200, y: 60, flip: FLIP_H, e.baseAddress! + 87 + phoenixFrame)
+		GL.polyFmt(POLY_ALPHA(20) | UInt32(POLY_CULL_NONE.rawValue) | POLY_ID(1))
+		GL.color(rgb15(0, 31, 20))
+		GL2D.sprite(x: 200, y: 90, flip: FLIP_V, e.baseAddress! + 87 + phoenixFrame)
+		GL.color(rgb15(0, 0, 0))
+		GL2D.sprite(x: 200, y: 130, flip: FLIP_V | FLIP_H, e.baseAddress! + 87 + phoenixFrame)
 	}
-	glColor(rgb15(31, 31, 31))
-	glPolyFmt(POLY_ALPHA(31) | UInt32(POLY_CULL_NONE.rawValue))
+	GL.color(rgb15(31, 31, 31))
+	GL.polyFmt(POLY_ALPHA(31) | UInt32(POLY_CULL_NONE.rawValue))
 	shuttle.withUnsafeBufferPointer {
-		glSpriteRotate(sx, sy, angle - (BRAD_PI / 2), Int32(GL_FLIP_NONE.rawValue), $0.baseAddress)
+		GL2D.spriteRotate(x: sx, y: sy, angle: angle - (BRAD_PI / 2), flip: FLIP_NONE, $0.baseAddress!)
 	}
-	glPolyFmt(POLY_ALPHA(20) | UInt32(POLY_CULL_NONE.rawValue) | POLY_ID(2))
+	GL.polyFmt(POLY_ALPHA(20) | UInt32(POLY_CULL_NONE.rawValue) | POLY_ID(2))
 	flyer.withUnsafeBufferPointer {
-		glSpriteRotateScaleXY(128, 96, frame &* 140, slerp(frame &* 120) &* 3, slerp(frame &* 210) &* 2,
-		                      Int32(GL_FLIP_NONE.rawValue), $0.baseAddress)
+		GL2D.spriteRotateScaleXY(x: 128, y: 96, angle: frame &* 140, scaleX: slerp(frame &* 120) &* 3, scaleY: slerp(frame &* 210) &* 2,
+		                         flip: FLIP_NONE, $0.baseAddress!)
 	}
-	glPolyFmt(POLY_ALPHA(31) | UInt32(POLY_CULL_NONE.rawValue))
-	glEnd2D()
+	GL.polyFmt(POLY_ALPHA(31) | UInt32(POLY_CULL_NONE.rawValue))
+	GL2D.end2D()
 
-	glFlush(0)
-	threadWaitForVBlank()
-	scanKeys()
-	if keysDown() & KEY_START != 0 { break }
+	GL.flush()
+	System.waitForVBlank()
+	Keys.scan()
+	if Keys.down.contains(.start) { break }
 }
